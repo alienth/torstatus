fedc34d7e31eabd3098e22e69694a4589a588a06
Jeremy Fehr <jfehr@wesleyan.edu>
2011-06-07 19:03:06 -0400

    Cleaned up the file structure a bit, got static images working, and cleaned up the index page.

--- statusapp/urls.py
+++ statusapp/urls.py
@@ -6,8 +6,10 @@ from django.conf import settings
 urlpatterns = patterns('',
 	(r'^$', 'statusapp.views.index'),
     (r'^flags/(?P<country_code>\w\w).gif', 'statusapp.views.displayFlag'),
-    
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': '/home/jfehr/status/statusapp/static'}) # Needs to be whole path
+
+    #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
