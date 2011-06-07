834292d213b0435c0ce3eaf1bc0ea8b1341a8685
Vlad Burca <vlad.burca@trincoll.edu>
2011-06-07 17:00:48 -0400

    Fixed the path to the CSS file so now the template should be displayed properly.

--- statusapp/urls.py
+++ statusapp/urls.py
@@ -6,8 +6,7 @@ from django.conf import settings
 urlpatterns = patterns('',
 	(r'^$', 'statusapp.views.index'),
     (r'^flags/(?P<country_code>\w\w).gif', 'statusapp.views.displayFlag'),
-    
-    #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
