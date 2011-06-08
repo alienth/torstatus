81bcbb495026998b514d5a88b4202652ec39641c
Vlad Burca <vlad.burca@trincoll.edu>
2011-06-08 18:23:59 -0400

    Templates update - Fixed a bug with the status icons and added sorting tables.

--- statusapp/views.py
+++ statusapp/views.py
@@ -9,5 +9,6 @@ def index(request):
 	statusEntry_FullList = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
 	clientAddress = request.META['REMOTE_ADDR']
 	template_values = {'statusEntry_FullList': statusEntry_FullList, 'clientAddress': clientAddress}
+	
 	return render_to_response('index.html', template_values)
 
