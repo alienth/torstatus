637b1596a29d2fa391db08ce88ba6ac69cfcd172
Vlad Burca <vlad.burca@trincoll.edu>
2011-06-08 14:31:40 -0400

    Updated the template - highlight on lines and some more CSS features.

--- statusapp/views.py
+++ statusapp/views.py
@@ -1,15 +1,13 @@
 from statusapp.models import Statusentry, Descriptor
 from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+
 
 import os
 
 def index(request):
 	statusEntry_FullList = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
-	descriptorDictionary = {}
-	for relay in statusEntry_FullList:
-		querySet = Descriptor.objects.get(descriptor="3c44166e2e872bc8348415f672db6849d5d64d50")
-		descriptorDictionary[relay] = querySet
-	template_values = {'statusEntry_FullList': statusEntry_FullList, 'descriptorDictionary': descriptorDictionary}
+	clientAddress = request.META['REMOTE_ADDR']
+	template_values = {'statusEntry_FullList': statusEntry_FullList, 'clientAddress': clientAddress}
 	return render_to_response('index.html', template_values)
 
