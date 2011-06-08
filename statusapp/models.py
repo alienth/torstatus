da20c0176ec7644f9049b07d52673fe1644270bd
Jeremy Fehr <jfehr@wesleyan.edu>
2011-06-08 11:27:29 -0400

    Added comments in models.py, which will soon be completely rewritten from scratch; added additional functionality and columns in index.html.

--- statusapp/models.py
+++ statusapp/models.py
@@ -14,14 +14,16 @@ class Consensus(models.Model):
     rawdesc = models.TextField()
     class Meta:
         db_table = u'consensus'
+    def __unicode__(self):
+        return str(self.validafter)
 
 class Statusentry(models.Model):
-    validafter = models.DateTimeField(primary_key=True)
-    nickname = models.CharField(max_length=57)
-    fingerprint = models.CharField(max_length=120)
-    descriptor = models.CharField(max_length=120)
+    validafter = models.DateTimeField(primary_key=True) # Issue: not unique
+    nickname = models.CharField(max_length=19)
+    fingerprint = models.CharField(max_length=40) # Setting this as a primary key wouldn't make it unique, either.
+    descriptor = models.CharField(max_length=40) # descriptor and fingerprint are not the same (at all!)!
     published = models.DateTimeField()
-    address = models.CharField(max_length=45)
+    address = models.CharField(max_length=15)
     orport = models.IntegerField()
     dirport = models.IntegerField()
     isauthority = models.IntegerField()
@@ -38,32 +40,32 @@ class Statusentry(models.Model):
     isvalid = models.IntegerField()
     isv2dir = models.IntegerField()
     isv3dir = models.IntegerField()
-    version = models.CharField(max_length=150, blank=True)
+    version = models.CharField(max_length=50, blank=True)
     bandwidth = models.BigIntegerField(null=True, blank=True)
     ports = models.TextField(blank=True)
     rawdesc = models.TextField()
 
     class Meta:
         db_table = u'statusentry'
-        unique_together = ("validafter", "fingerprint") # May or may not be necessary
+        unique_together = ("validafter", "fingerprint") # May or may not be necessary. Would it be possible/useful to use such a tuple as an int to be used as a primary key?
     def __unicode__(self):
         return self.fingerprint
 
 class Descriptor(models.Model):
-    #statusentry = models.ForeignKey('Statusentry', related_name="statusapp_statusentry_related") # A guess
-    descriptor = models.CharField(max_length=120, primary_key=True)
-    nickname = models.CharField(max_length=57)
-    address = models.CharField(max_length=45)
+    #statusentry = models.ForeignKey('Statusentry', related_name="statusapp_statusentry_related") # A guess... something like this should definitely exist...
+    descriptor = models.CharField(max_length=40, primary_key=True) # Unique. Good.
+    nickname = models.CharField(max_length=19)
+    address = models.CharField(max_length=15)
     orport = models.IntegerField()
     dirport = models.IntegerField()
-    fingerprint = models.CharField(max_length=120)
+    fingerprint = models.CharField(max_length=40)
     bandwidthavg = models.BigIntegerField()
     bandwidthburst = models.BigIntegerField()
     bandwidthobserved = models.BigIntegerField()
-    platform = models.CharField(max_length=768, blank=True)
+    platform = models.CharField(max_length=256, blank=True)
     published = models.DateTimeField()
     uptime = models.BigIntegerField(null=True, blank=True)
-    extrainfo = models.CharField(max_length=120, blank=True)
+    extrainfo = models.CharField(max_length=40, blank=True)
     rawdesc = models.TextField()
     class Meta:
         db_table = u'descriptor'
