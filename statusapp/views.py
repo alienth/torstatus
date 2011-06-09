e494116e0413add25bc5df85064a706d51ecda6b
Vlad Burca <vlad.burca@trincoll.edu>
2011-06-09 10:19:41 -0400

    Fixed the OS icons; display IP (hostname); created new column for contries (sortable). Cleaning the CSS...

--- statusapp/views.py
+++ statusapp/views.py
@@ -7,8 +7,14 @@ import os
 
 def index(request):
 	statusEntry_FullList = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
+	descriptor_list = []
+	for entry in statusEntry_FullList:
+		try:
+			descriptor_list.append(Descriptor.objects.get(pk=entry.descriptor))
+		except:
+			descriptor_list.append(Descriptor())
+	statusEntry_descriptorEntry = zip(statusEntry_FullList, descriptor_list)
 	clientAddress = request.META['REMOTE_ADDR']
-	template_values = {'statusEntry_FullList': statusEntry_FullList, 'clientAddress': clientAddress}
-	
+	template_values = {'statusEntry_descriptorEntry': statusEntry_descriptorEntry, 'clientAddress': clientAddress, }
 	return render_to_response('index.html', template_values)
 
