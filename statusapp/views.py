8c078d6cfec1c6f0082985aaeee591a50d4ab3f0
Jeremy Fehr <jfehr@wesleyan.edu>
2011-06-09 16:21:00 -0400

    Wrote a basic view for the relay details page. Raw SQL queries are not currently used in this method, but probably should be eventually. The same goes for the index view. The index view should also be rewritten so that instead of passing a null descriptor, a number of recent statusentries are searched for a matching descriptor. This could be done by creating a helper function that could be used in both the details and index methods.

--- statusapp/views.py
+++ statusapp/views.py
@@ -16,5 +16,41 @@ def index(request):
 	return render_to_response('index.html', template_values)
 
 def details(request, fingerprint):
-    template_values = {'fingerprint': fingerprint,}
+    #import datetime.datetime
+    #import datetime.timedelta
+
+    statuses = Statusentry.objects.filter(fingerprint=fingerprint)
+    # This next block isn't working quite right, but it's my shot at a raw SQL 
+    # query. This needs to be fixed up, see below.
+    #oldest_tolerable = datetime.now() - timedelta(hours=72)
+    #oldest_tol_str = str(oldest_tolerable.replace(microsecond=0))
+    #recent_statuses = statuses.extra(select={'is_recent': "validafter > '" + oldest_tol_str + "'"}) 
+    #recent_statuses = recent_statuses.extra(order_by = ['-is_recent'])
+    #statusentry = recent_statuses.latest('validafter')
+
+    status_count = int(statuses.count()) # int() for MySQL/PostgreSQL compatibility
+    # Doesn't work -- there is no default ordering
+    #recent_statuses = statuses.reverse()[max(-1, (status_count - 73)):status_count]
+
+    recent_statuses = statuses[max(-1, (status_count - 73)):status_count]
+
+    # This next line is extremely inefficient, but no ordering is defined so
+    # there seems to be no other way... if we had experience doing raw SQL 
+    # queries through django, this would be one good place to implement them.
+    recent_statuses_list = list(recent_statuses)
+    recent_statuses_list.reverse()
+
+    descriptor = None
+    i = 0
+    while ((descriptor == None) & (i < 72)):
+        try:
+            descriptor = Descriptor.objects.get(descriptor=recent_statuses_list[i].descriptor)
+        except:
+            i += 1
+    
+    if (i == 72):
+        i = 0
+
+    status_and_descriptor = (recent_statuses[i], descriptor)
+    template_values = {'status_and_descriptor': status_and_descriptor}
     return render_to_response('details.html', template_values)
