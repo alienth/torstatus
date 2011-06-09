d5a3a1dc85440d02129c766fa5d399d5f8a5fa8a
Jeremy Fehr <jfehr@wesleyan.edu>
2011-06-09 10:33:17 -0400

    Statusentries and descriptors are now accessible by index.html.

--- statusapp/views.py
+++ statusapp/views.py
@@ -2,12 +2,21 @@ from statusapp.models import Statusentry, Descriptor
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpRequest
 
-
-import os
-
 def index(request):
-	statusEntry_FullList = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
-	clientAddress = request.META['REMOTE_ADDR']
-	template_values = {'statusEntry_FullList': statusEntry_FullList, 'clientAddress': clientAddress}
-	return render_to_response('index.html', template_values)
+    statusentries = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
+
+    descriptors = []
+    for statusentry in statusentries:
+        try:
+            descriptors.append(Descriptor.objects.get(descriptor=statusentry.descriptor))
+        except:
+            descriptors.append(Descriptor())
+
+    statuses_descriptors = zip(statusentries, descriptors)
+
+    clientAddress = request.META['REMOTE_ADDR']
+    
+    template_values = {'statuses_descriptors': statuses_descriptors, 'clientAddress': clientAddress}
+    
+    return render_to_response('index.html', template_values)
 
