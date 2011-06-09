22e56599f0f75c0d8f5ddf18b07fceec895b44fb
Diego Calderon <dcalderon@wesleyan.edu>
2011-06-09 10:42:11 -0400

    We found that address is actually saved in the statusentry so we don't even need the descriptor to access it. Also having some trouble with looking up hostnames, but we figured we could just use GEOip later

--- statusapp/views.py
+++ statusapp/views.py
@@ -1,6 +1,8 @@
 from statusapp.models import Statusentry, Descriptor
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpRequest
+import sys
+import socket
 
 
 import os
@@ -10,9 +12,9 @@ def index(request):
     descriptor_list = []
     for entry in statusEntry_FullList:
         try:
-            descriptor_list.append(Descriptor.objects.get(pk=entry.descriptor))
+            temp = Descriptor.objects.get(pk=entry.descriptor)
+            descriptor_list.append(temp)
         except:
             descriptor_list.append(Descriptor())
     template_values = zip(statusEntry_FullList, descriptor_list)
     return render_to_response('index.html', {'template_values': template_values})
-
