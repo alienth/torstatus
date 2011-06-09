3b0d3d8b1d087ccdc19e66d8400cd334ec3707c1
Vlad Burca <vlad.burca@trincoll.edu>
2011-06-09 11:00:16 -0400

    Cleaned CSS

--- statusapp/views.py
+++ statusapp/views.py
@@ -2,9 +2,6 @@ from statusapp.models import Statusentry, Descriptor
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpRequest
 
-
-import os
-
 def index(request):
 	statusEntry_FullList = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
 	descriptor_list = []
@@ -17,4 +14,4 @@ def index(request):
 	clientAddress = request.META['REMOTE_ADDR']
 	template_values = {'statusEntry_descriptorEntry': statusEntry_descriptorEntry, 'clientAddress': clientAddress, }
 	return render_to_response('index.html', template_values)
-
+    
