787787c7d4a315edab066a206a7f635c6b3ee6ae
Jeremy Fehr <jfehr@wesleyan.edu>
2011-06-14 12:30:12 -0400

    The view for the index page now correctly displays all relays that show up in consensuses in the last 24 hours.

--- statusapp/views.py
+++ statusapp/views.py
@@ -3,38 +3,48 @@ from django.shortcuts import render_to_response #get_object_or_404
 from django.http import HttpResponse, HttpRequest, Http404
 import csv
 
+# To do: get rid of javascript sorting: pass another argument
+# to this view function and sort the table accordingly.
 def index(request):
     """
-	statusEntry_FullList = Statusentry.objects.filter(pk='2011-05-31 19:00:00')
-	descriptor_list = []
-	for entry in statusEntry_FullList:
-		try:
-			descriptor_list.append(Descriptor.objects.get(pk=entry.descriptor))
-		except:
-			descriptor_list.append(Descriptor())
-	statusEntry_descriptorEntry = zip(statusEntry_FullList, descriptor_list)
-	clientAddress = request.META['REMOTE_ADDR']
-	template_values = {'statusEntry_descriptorEntry': statusEntry_descriptorEntry, 'clientAddress': clientAddress, }
-	return render_to_response('index.html', template_values)
+    Supply a dictionary to the index.html template consisting of keys
+    equivalent to columns in the statusentry and descriptor tables in the
+    database. Querying the database is done with raw SQL, and currently
+    only relays in the last consensus are found. This needs to be fixed
+    as soon as possible.
     """
+
     from django.db import connection
+    import datetime
 
     cursor = connection.cursor()
 
     cursor.execute('SELECT MAX(validafter) FROM statusentry')
 
-    last_validafter = cursor.fetchone()[0]
+    validafter_range = (cursor.fetchone()[0] - datetime.timedelta(hours=24))
 
-    cursor.execute('SELECT statusentry.isbadexit, statusentry.isnamed, \
-            statusentry.fingerprint, statusentry.nickname, \
-            descriptor.bandwidthobserved, descriptor.uptime, \
-            statusentry.address, statusentry.isfast, statusentry.isexit, \
-            statusentry.isguard, statusentry.isstable, statusentry.isauthority, \
-            descriptor.platform, statusentry.orport, statusentry.dirport FROM \
-            statusentry LEFT JOIN descriptor ON statusentry.descriptor = \
-            descriptor.descriptor WHERE statusentry.validafter = %s', \
-            [last_validafter]) # Ugly, yes, but functional -- misses FOUR
-                               # descriptors (out of 2256)
+    ## Problem: only gets routers published in the last consensus, not in
+    ## the last 24 hours.
+
+    # New Problem: Query takes a LONG time (7.96 sec on marlow). 
+    # cache is NECESSARY!
+
+    # Other Problem: this is even uglier.
+    cursor.execute('SELECT sentry.isbadexit, sentry.isnamed, \
+            sentry.fingerprint, sentry.nickname, descriptor.bandwidthobserved, \
+            descriptor.uptime, sentry.address, sentry.isfast, sentry.isexit, \
+            sentry.isguard, sentry.isstable, sentry.isauthority, \
+            descriptor.platform, sentry.orport, sentry.dirport FROM \
+            descriptor RIGHT JOIN (SELECT u.isbadexit, u.isnamed, \
+            u.fingerprint, u.nickname, u.address, u.isfast, u.isexit, \
+            u.isguard, u.isstable, u.isauthority, u.orport, u.dirport, \
+            u.descriptor, q.validafter FROM statusentry AS u JOIN \
+            (SELECT nickname, MAX(validafter) AS validafter FROM statusentry \
+            WHERE validafter > %s GROUP BY nickname) AS q \
+            ON u.nickname = q.nickname AND u.validafter = q.validafter WHERE \
+            u.validafter > %s) as sentry ON \
+            sentry.descriptor = descriptor.descriptor;', \
+            [validafter_range, validafter_range])
 
     relays = cursor.fetchall()
     client_address = request.META['REMOTE_ADDR']
@@ -43,25 +53,11 @@ def index(request):
 
 def details(request, fingerprint):
     """
-    statuses = Statusentry.objects.filter(fingerprint=fingerprint)
-    status_count = int(statuses.count())
-    recent_statuses = statuses[max(-1, (status_count - 73)):status_count]
-    recent_statuses_list = list(recent_statuses)
-    recent_statuses_list.reverse()
-
-    descriptor = None
-    i = 0
-    while ((descriptor == None) & (i < 72)):
-        try:
-            descriptor = Descriptor.objects.get(descriptor=recent_statuses_list[i].descriptor)
-        except:
-            i += 1
-    
-    if (i == 72):
-        i = 0
-    template_values = {'status': recent_statuses[i], 'descriptor': descriptor}
-    return render_to_response('details.html', template_values)
+    Supply a dictionary to the details.html template consisting of keys
+    equivalent to columns in the statusentry and descriptor tables in the
+    database. Querying the database is done with raw SQL.
     """
+
     from django.db import connection
 
     cursor = connection.cursor()
@@ -101,23 +97,39 @@ def details(request, fingerprint):
     return render_to_response('details.html', template_values)
 
 def exitnodequery(request):
+    """
+    """
+
+    # For now, this function is just a placeholder.
     variables = "MWAHAHA"
     template_values = {'variables': variables,}
     return render_to_response('nodequery.html', template_values)
 
 def networkstatisticgraphs(request):
+    """
+    """
+
+    # For now, this function is just a placeholder.
     variables = "mWAHAHA"
     template_values = {'variables': variables,}
     return render_to_response('statisticgraphs.html', template_values)
 
 def columnpreferences(request):
+    """
+    """
+
+    # For now, this function is just a placeholder.
     variables = "SOMETHING"
     template_values = {'variables': variables,}
     return render_to_response('columnpreferences.html', template_values)
 
-UNRULY_PASSENGERS = [146,184,235,200,226,251,299,273,281,304,203]
-
 def unruly_passengers_csv(request):
+    """
+    """
+
+    # For now, this function is just a placeholder.
+    UNRULY_PASSENGERS = [146,184,235,200,226,251,299,273,281,304,203]
+    
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename=test_data.csv'
