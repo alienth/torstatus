f4c821792c97558e2e54bb0b4177ca78dd25e4fa
Diego Calderon <dcalderon@wesleyan.edu>
2011-06-21 10:51:50 -0400

    Filters need to be fixed. Pulling Jeremy's corrected filters

--- settings.py
+++ settings.py
@@ -19,10 +19,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3', or 'oracle'.
-        'NAME': 'tordir',                      # Or path to database file if using sqlite3.
-        'USER': 'metrics',                      # Not used with sqlite3.
-        'PASSWORD': 'meiD7go3',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3', or 'oracle'.
+        'NAME': 'dcalderon',                      # Or path to database file if using sqlite3.
+        'USER': 'dcalderon',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
