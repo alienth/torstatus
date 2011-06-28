959a09655009ab0a3403474be1d1d82af481d042
Diego Calderon <dcalderon@wesleyan.edu>
2011-06-28 12:53:03 -0400

    I just implemented the csv download functionality. There might be a few bugs but the basic data needed is there.

--- status/statusapp/urls.py
+++ status/statusapp/urls.py
@@ -11,14 +11,14 @@ urlpatterns = patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', \
             {'document_root': settings.MEDIA_ROOT}),
     (r'^exit-node-query/', 'statusapp.views.exitnodequery'),
+    (r'^network-statistic-graphs/aggregatesummary.png', 'statusapp.views.aggregatesummary'),
     (r'^network-statistic-graphs/', 'statusapp.views.networkstatisticgraphs'),
     (r'^column-preferences/', 'statusapp.views.columnpreferences'),
-    (r'^Tor-Query-Export.csv','statusapp.views.unruly_passengers_csv'),
+    (r'^Tor-Query-Export.csv','statusapp.views.current_results_csv'),
     #Eventually have to make own function to send appropriate stuff
-    (r'^Tor-IP-List-All.csv','statusapp.views.unruly_passengers_csv'),#ditto
-    (r'^Tor-IP-List-Exit.csv','statusapp.views.unruly_passengers_csv'),#ditto
+    (r'^Tor-IP-List-All.csv','statusapp.views.all_ip_csv'),#ditto
+    (r'^Tor-IP-List-Exit.csv','statusapp.views.all_exit_csv'),#ditto
     (r'^custom/$', 'statusapp.views.custom_index'),
-
     #Figure out how to make a method or methods that is capable of
     #taking all the custom parameters and generate a custom index page.
 )
--- status/statusapp/views.py
+++ status/statusapp/views.py
@@ -82,7 +82,6 @@ def index(request):
 
     if queryOptions:
         if queryOptions['isauthority'] == 'yes':
-<<<<<<< HEAD
             a = a.filter(isauthority=1)
         elif queryOptions['isauthority'] == 'no': 
             a = a.filter(isauthority=0)
@@ -271,32 +270,234 @@ def exitnodequery(request):
             dest_ip_valid, 'dest_port_valid': dest_port_valid}
     return render_to_response('nodequery.html', template_values)
 
-def unruly_passengers_csv(request):
-    # For now, this function is just a placeholder. We're using this to see
-    # if we can understand the csv module.
-    """
-    """
-    UNRULY_PASSENGERS = [146,184,235,200,226,251,299,273,281,304,203]
-    
-    # Create the HttpResponse object with the appropriate CSV header.
-    response = HttpResponse(mimetype='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=test_data.csv'
+def current_results_csv(request):
 
-    # Create the CSV writer using the HttpResponse as the "file."
+    currentColumns = []
+    if not ('currentColumns' in request.session):
+        currentColumns = ["Country Code", "Uptime", "ORPort", "DirPort", "IP", \
+                    "Exit", "Authority", "Fast", "Guard", "Stable", \
+                    "Bandwidth", "V2Dir", "Platform", "BadExit", "Named"]
+        request.session['currentColumns'] = currentColumns
+    else:
+        currentColumns = request.session['currentColumns']
+
+        currentColumns.remove("Hostname")
+        currentColumns.remove("Valid")
+        currentColumns.remove("Running")
+        currentColumns.remove("Hibernating")
+        currentColumns.remove("Named")
+        currentColumns.append("Nickname")
+
+    last_va = Statusentry.objects.aggregate(last=Max('validafter'))['last']
+    a = Statusentry.objects.filter(validafter=last_va).extra(select={'geoip': 'geoip_lookup(address)'}).order_by('nickname')
+    
+    #MIGHT WORKS BUT DOESN'T SORT BY CERTAIN PARAMATERS SUCH AS COUNTRY
+    #if 'sortListings' in request.GET:
+    #    a = Statusentry.objects.filter(validafter=last_va).extra(select={'geoip': 'geoip_lookup(address)'}).order_by(request.GET['sortListings'])
+    #else:
+    #    a = Statusentry.objects.filter(validafter=last_va).extra(select={'geoip': 'geoip_lookup(address)'}).order_by('nickname')
+    
+    #############################################################
+    queryOptions = {}
+    if (request.GET):
+        if ('resetQuery' in request.GET):
+            if ('queryOptions' in request.session):
+                del request.session['queryOptions']
+        else:
+            queryOptions = request.GET    
+            request.session['queryOptions'] = queryOptions    
+    if (not queryOptions and 'queryOptions' in request.session):
+            queryOptions = request.session['queryOptions']
+
+    if queryOptions:
+        if queryOptions['isauthority'] == 'yes':
+            a = a.filter(isauthority=1)
+        elif queryOptions['isauthority'] == 'no': 
+            a = a.filter(isauthority=0)
+        if queryOptions['isbaddirectory'] == 'yes':
+            a = a.filter(isbaddirectory=1)
+        elif queryOptions['isbaddirectory'] == 'no':  
+            a = a.filter(isbaddirectory=0)
+        if queryOptions['isbadexit'] == 'yes':
+            a = a.filter(isbadexit=1)
+        elif queryOptions['isbadexit'] == 'no': 
+            a = a.filter(isbadexit=0)
+        if queryOptions['isexit'] == 'yes':
+            a = a.filter(isexit=1)
+        elif queryOptions['isexit'] == 'no': 
+            a = a.filter(isexit=0)
+        '''
+        if queryOptions['ishibernating'] == 'yes':
+            a = a.filter(ishibernating=1)
+        elif queryOptions['ishibernating'] == 'no': 
+            a = a.filter(ishibernating=0)
+        '''
+        if queryOptions['isnamed'] == 'yes':
+            a = a.filter(isnamed=1)
+        elif queryOptions['isnamed'] == 'no': 
+            a = a.filter(isnamed=0)
+        if queryOptions['isstable'] == 'yes':
+            a = a.filter(isstable=1)
+        elif queryOptions['isstable'] == 'no': 
+            a = a.filter(isstable=0)
+        if queryOptions['isrunning'] == 'yes':
+            a = a.filter(isrunning=1)
+        elif queryOptions['isrunning'] == 'no': 
+            a = a.filter(isrunning=0)
+        if queryOptions['isvalid'] == 'yes':
+            a = a.filter(isvalid=1)
+        elif queryOptions['isvalid'] == 'no': 
+            a = a.filter(isvalid=0)
+        if queryOptions['isv2dir'] == 'yes':
+            a = a.filter(isv2dir=1)
+        elif queryOptions['isv2dir'] == 'no': 
+            a = a.filter(isv2dir=0)
+    #############################################################
+
+    #Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=current_results.csv'
+
+    ########################################################
+    #
+    #   We need to check for which columns are being searched for and
+    #   get the data from each one into a dictionary.
+    #
+    
+    rows = {}
+    headers = {}
+    for column in currentColumns:
+        rows[column] = []
+
+    for entry in a:
+        rows["Nickname"].append(entry.nickname)
+
+    if "Country Code" in currentColumns:
+        for entry in a:
+            rows["Country Code"].append(entry.geoip.split(',')[0][1:3])
+
+    if "Bandwidth" in currentColumns:
+        for entry in a:
+            rows["Bandwidth"].append(entry.descriptorid.bandwidthobserved)
+
+    if "Uptime" in currentColumns:
+        for entry in a:
+            rows["Uptime"].append(entry.descriptorid.uptime)
+
+    if "IP" in currentColumns:
+        for entry in a:
+            rows["IP"].append(entry.address)
+
+    if "Fingerprint" in currentColumns:
+        for entry in a:
+            rows["Fingerprint"].append(entry.fingerprint)
+
+    if "LastDescriptorPublished" in currentColumns:
+        for entry in a:
+            rows["LastDescriptorPublished"].append(entry.published)
+
+    if "Contact" in currentColumns:
+        for entry in a:
+            rows["Contact"].append(_contact(entry.descriptorid.rawdesc))
+
+    if "BadDir" in currentColumns:
+        for entry in a:
+            rows["BadDir"].append(entry.isbaddirectory)
+
+    if "DirPort" in currentColumns:
+        for entry in a:
+            rows["DirPort"].append(entry.dirport)
+
+    if "Exit" in currentColumns:
+        for entry in a:
+            rows["Exit"].append(entry.isexit)
+
+    if "Authority" in currentColumns:
+        for entry in a:
+            rows["Authority"].append(entry.isauthority)
+
+    if "Fast" in currentColumns:
+        for entry in a:
+            rows["Fast"].append(entry.isfast)
+
+    if "Guard" in currentColumns:
+        for entry in a:
+            rows["Guard"].append(entry.isguard)
+
+    if "V2Dir" in currentColumns:
+        for entry in a:
+            rows["V2Dir"].append(entry.isv2dir)
+
+    if "Platform" in currentColumns:
+        for entry in a:
+            rows["Platform"].append(entry.descriptorid.platform)
+
+    if "Stable" in currentColumns:
+        for entry in a:
+            rows["Stable"].append(entry.isstable)
+
+    if "ORPort" in currentColumns:
+        for entry in a:
+            rows["ORPort"].append(entry.orport)
+
+    if "BadExit" in currentColumns:
+        for entry in a:
+            rows["BadExit"].append(entry.isbadexit)
+
+    fieldnames = currentColumns
     writer = csv.writer(response)
-    writer.writerow(['Year', 'Unruly Airline Passengers'])
-    for (year, num) in zip(range(1995, 2006), UNRULY_PASSENGERS):
-        writer.writerow([year, num])
+    writer = csv.DictWriter(response, fieldnames=fieldnames)
+    for n in fieldnames:
+        headers[n] = n
+    writer.writerow(headers)
+    for i in range(0, len(rows[currentColumns[0]])):
+        dict_row = {}
+        for column in currentColumns:
+            dict_row[column] = rows[column][i]
+        writer.writerow(dict_row)
+    return response
+
+def all_ip_csv(request):
+
+    last_va = Statusentry.objects.aggregate(last=Max('validafter'))['last']
+    a = Statusentry.objects.filter(validafter=last_va)
+    
+    IPs = []
+    for entry in a:
+        IPs.append(entry.address)
+
+    response = HttpResponse(mimetype= 'text/csv')
+    response['Content-Disposition'] = 'attachment; filename=all_ips_csv'
+
+    writer = csv.writer(response)
+    for ip in IPs:
+        writer.writerow([ip])
+    return response
+
+def all_exit_csv(request):
+    
+    last_va = Statusentry.objects.aggregate(last=Max('validafter'))['last']
+    all_entries = Statusentry.objects.filter(validafter=last_va)
+    a = all_entries.filter(isexit=True)
+    exit_IPs = []
+
+    for entry in a:
+        exit_IPs.append(entry.address)
+
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=all_exit_nodes.csv'
+
+    writer = csv.writer(response)
+    for ip in exit_IPs:
+        writer.writerow([ip])
+
     return response
 
 def networkstatisticgraphs(request):
     #TODO
     # For now, this function is just a placeholder.
 
-    variables = "TEMP STRING"
-    template_values = {'variables': variables}
-    return render_to_response('statisticgraphs.html', template_values)
-
+    return render_to_response('statisticgraphs.html')
 
 def columnpreferences(request):
     '''
@@ -838,3 +1039,100 @@ def readhist(request, fingerprint):
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response, ha="center")
     return response
+
+def aggregatesummary(request):
+    """
+    Return a graph representing an aggregate summary of the routers on
+    the network as an HttpResponse object.
+
+    @rtype: HttpResponse
+    @return: A graph representing an aggregate summary of the routers on
+        the Tor network.
+    """
+    import matplotlib
+    from matplotlib.backends.backend_agg import \
+            FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    # Draw the graph such that the labels and title are visible,
+    # and remove excess whitespace.
+    matplotlib.rcParams['figure.subplot.left'] = 0.04
+    matplotlib.rcParams['figure.subplot.right'] = 0.99
+    matplotlib.rcParams['figure.subplot.top'] = 0.95
+    matplotlib.rcParams['figure.subplot.bottom'] = 0.04
+
+    last_va = Statusentry.objects.aggregate(\
+            last=Max('validafter'))['last']
+
+    statusentries = Statusentry.objects.filter(validafter=last_va)
+
+    total = statusentries.count()
+    counts = Statusentry.objects.filter(validafter=last_va).aggregate(\
+            isauthority=CountCase('isauthority', when=True),
+            isbaddirectory=CountCase('isbaddirectory', when=True),
+            isbadexit=CountCase('isbadexit', when=True),
+            isexit=CountCase('isexit', when=True),
+            isfast=CountCase('isfast', when=True),
+            isguard=CountCase('isguard', when=True),
+            isnamed=CountCase('isnamed', when=True),
+            isstable=CountCase('isstable', when=True),
+            isrunning=CountCase('isrunning', when=True),
+            isvalid=CountCase('isvalid', when=True),
+            isv2dir=CountCase('isv2dir', when=True))
+
+    keys = ['isauthority', 'isbaddirectory', 'isbadexit', 'isexit',
+            'isfast', 'isguard', 'isnamed', 'isstable', 'isrunning',
+            'isvalid', 'isv2dir']
+    labels = ['Total', 'Authority', 'BadDirectory', 'BadExit', 'Exit',
+            'Fast', 'Guard', 'Named', 'Stable', 'Running', 'Valid',
+            'V2Dir']
+    num_params = len(labels)
+    xs = range(num_params)
+    x_index = matplotlib.numpy.arange(num_params)
+
+    ys = []
+    ys.append(total)
+    for count in keys:
+        ys.append(counts[count])
+
+    fig = Figure(facecolor='white', edgecolor='black', figsize=(12, 6),
+            frameon=False)
+    ax = fig.add_subplot(111)
+
+    bar_width = 0.5
+    ax.bar(xs, ys, color='#66CD00', width=bar_width)
+    for i in range(num_params):
+        ax.text(xs[i] + (bar_width / 2.0), ys[i] + 10, str(ys[i]),
+                fontsize='9', horizontalalignment='center')
+    ax.set_xticks(x_index + (bar_width / 2.0))
+    ax.set_xticklabels(labels, fontsize='9')
+
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label1.set_fontsize('9')
+
+    ax.set_title("Aggregate Summary -- Number of Routers Matching "
+            + "Specified Criteria", fontsize='12')
+
+    canvas = FigureCanvas(fig)
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response, ha="center")
+    return response
+
+def _contact(rawdesc):
+    """
+    Get the contact information of a relay from its raw descriptor.
+
+    It is possible that a relay will not publish any contact information.
+    In this case, "No contact information given" is returned.
+
+    @type rawdesc: C{string} or C{buffer}
+    @param rawdesc: The raw descriptor of a relay.
+    @rtype: C{string}
+    @return: The contact information of the relay.
+    """
+
+    for line in str(rawdesc).split("\n"):
+        if (line.startswith("contact")):
+            return line[8:]
+    return "No contact information given"
