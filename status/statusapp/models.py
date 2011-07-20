3e59deb06715b6b3ab7d01ae63723c4d8ac3591d
Jeremy Fehr <jfehr@wesleyan.edu>
2011-07-20 12:12:45 -0400

    A few cache.sql improvements and a model change.

--- status/statusapp/models.py
+++ status/statusapp/models.py
@@ -1140,9 +1140,15 @@ class ActiveDescriptor(models.Model):
     bandwidthavg = models.BigIntegerField()
     bandwidthburst = models.BigIntegerField()
     bandwidthobserved = models.BigIntegerField()
-    platform = models.CharField(max_length=256)
+    bandwidthkbps = models.BigIntegerField()
     uptime = models.BigIntegerField()
-    rawdesc = models.TextField()
+    uptimedays = models.BigIntegerField()
+    platform = models.CharField(max_length=256)
+    contact = models.TextField()
+    onionkey = models.CharField(max_length=188)
+    signingkey = models.CharField(max_length=188)
+    exitpolicy = models.TextField()
+    family = models.TextField()
 
     class Meta:
         verbose_name = "active descriptor"
@@ -1279,7 +1285,7 @@ class ActiveRelay(models.Model):
     uptime = models.BigIntegerField(blank=True)
     uptimedays = models.BigIntegerField(blank=True)
     platform = models.CharField(max_length=256, blank=True)
-    contact = models.CharField(max_length=96, blank=True)
+    contact = models.TextField()
     onionkey = models.CharField(max_length=188, blank=True)
     signingkey = models.CharField(max_length=188, blank=True)
     exitpolicy = models.TextField(blank=True)
