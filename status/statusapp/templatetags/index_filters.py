f39b22a03b6f559e3dd2bfbc66ffb34b7b24ab4f
Vlad Burca <vlad.burca@trincoll.edu>
2011-07-26 15:40:17 -0400

    Added documentation to some template filters and helpers. Cleaned some of the unused code.

--- status/statusapp/templatetags/index_filters.py
+++ status/statusapp/templatetags/index_filters.py
@@ -77,6 +77,14 @@ def percent(a, b):
 @register.filter
 def key(d, key_name):
     """
+    Return the value of a key in a dictionary.
+    
+    @type d: C{dict}
+    @param d: The given dictionary.
+    @type key_name: C{string}
+    @param key_name: The given key.
+    @rtype: C{value}
+    @return: The value of the given key in the dictionary.
     """
     if key_name in d:
         return d[key_name]
@@ -91,17 +99,38 @@ def subtract(a, b):
     @param a: The number to subtract from.
     @type b: C{int}
     @param b: The number to subtract.
+    @rtype: C{int}
+    @return: The difference of the two given integers.
     """
     return a - b
     
 
 @register.filter
-def field_key(relay_dict, key_name):
+def field_value(relay_dict, key_name):
+    """
+    Return the value of a specific field of a relay.
+    
+    @type relay_dict: C{dict}
+    @param relay_dict: Dictionary that contains all the fields for a specific
+                    relay.
+    @type key_name: C{string}
+    @param key_name: The key of a field.
+    @rtype: C{value}
+    @return: The value of the given key in the relay dictionary.
+    """
     return relay_dict[FILTERED_NAME[key_name]]
 
 
 @register.filter
 def get_os(platform):
+    """
+    Return the icon-name of the OS.
+    
+    @type platform: C{string}
+    @param platform: A string that represents the platform of the relay.
+    @rtype: C{string}
+    @return: The icon-name version of the OS of the relay.
+    """
     if platform:
         for os in OS_LIST:
             if os in platform:
