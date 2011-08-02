2da47d678e42b048326689433347c9b666831f71
Diego Calderon <dcalderon@wesleyan.edu>
2011-08-02 14:51:05 -0400

    Fixed a minor bug in the index_filters

--- status/statusapp/templatetags/index_filters.py
+++ status/statusapp/templatetags/index_filters.py
@@ -1,114 +1,105 @@
 """
 Custom filters for the index page.
 """
+# Django-specific import statements -----------------------------------
 from django import template
 
+# INIT Variables ------------------------------------------------------
+FILTERED_NAME = {'Longitude': 'longitude',
+                 'Latitude': 'latitude',
+                 'Country Code': 'country',
+                 'Router Name': 'nickname',
+                 'Bandwidth': 'bandwidthkbps',
+                 'Uptime': 'uptime',
+                 'IP': 'address',
+                 'Hibernating': 'hibernating',
+                 'ORPort': 'orport',
+                 'DirPort': 'dirport',
+                 'BadExit': 'isbadexit',
+                 'Named': 'isnamed',
+                 'Exit': 'isexit',
+                 'Authority': 'isauthority',
+                 'Fast': 'isfast',
+                 'Guard': 'isguard',
+                 'Stable': 'isstable',
+                 'V2Dir': 'isv2dir',
+                 'Platform': 'platform',
+                 'Fingerprint': 'fingerprint',
+                 'LastDescriptorPublished': 'published',
+                 'Contact': 'contact',
+                 'BadDir': 'isbaddirectory',
+                }
+
+OS_LIST = set(('Linux', 'XP', 'Windows', 'Darwin', 'FreeBSD', 'NetBSD',
+            'OpenBSD', 'SunOS', 'IRIX', 'Cygwin', 'Dragon'))
+
 register = template.Library()
-
-# Moved to HELPERS
-@register.filter
-def kilobytes_ps(bytes_ps):
-    """
-    Convert a bandwidth value in bytes to a bandwidth value in kilobytes
-
-    @type bytes_ps: C{int}, C{float}, C{long}, or C{string}
-    @param bytes_ps: The bandwidth value, in bps.
-    @rtype: C{int}
-    @return: The bandwidth value in kbps.
-    """
-    # As statusapp.views.details is written now, this value can
-    # be None or an empty string sometimes.
-    if (bytes_ps == '' or bytes_ps is None):
-        return 0
-    else:
-        return int(bytes_ps) / 1024
-
-# Moved to HELPERS
-@register.filter
-def days(seconds):
-    """
-    Convert an duration in seconds to an uptime in days, rounding down.
-
-    @type seconds: C{int}, C{float}, C{long}, or C{string}
-    @param seconds: The duration in seconds.
-    @rtype: C{int}
-    @return: The duration in days.
-    """
-    # As statusapp.views.details is written now, this value can
-    # be None or an empty string sometimes.
-    if (seconds == '' or seconds is None):
-        return 0
-    else:
-        return int(seconds) / 86400
-
-# Moved to HELPERS
-@register.filter
-def country(location):
-    """
-    Get the country associated with a tuple as a string consisting of
-    a country, a latitude, and a longitude.
-
-    >>> getcountry('(US,-43.0156,68.2351)')
-    'US'
-
-    @type location: C{string}
-    @param location: A tuple consisting of a country, latitude, and
-        longitude as a string.
-    @rtype: C{string}
-    @return: The country code in the tuple as a string.
-    """
-    return location[1:3].lower()
-
-
-@register.filter
-def percent(a, b):
-    """
-    Return C{a / b} as a percent.
-
-    @type a: C{int} or C{NoneType}
-    @param a: The numerator of the percent.
-    @type b: C{int}
-    @param b: The denominator of the percent.
-    @rtype: C{string}
-    @return: C{a / b} as a percent as a string.
-    """
-    if a is None:
-        return '0.00%'
-    else:
-        return '%0.2f%%' % (100.0 * a / b)
-
-# Moved to HELPERS
-@register.filter
-def latitude(geoip):
-    """
-    Get the latitude from a GeoIP string.
-
-    @type geoip: C{string} or C{buffer}
-    @param geoip: A string formatted as a tuple with entries country
-        code, latitude, and longitude.
-    @rtype: C{string}
-    @return: The latitude associated with C{geoip}.
-    """
-    return str(geoip).split(',')[1]
-
-# Moved to HELPERS
-@register.filter
-def longitude(geoip):
-    """
-    Get the longitude from a GeoIP string.
-
-    @type geoip: C{string} or C{buffer}
-    @param geoip: A string formatted as a tuple with entries country
-        code, latitude, and longitude.
-    @rtype: C{string}
-    @return: The longitude associated with C{geoip}.
-    """
-    return str(geoip).strip('()').split(',')[2]
 
 
 @register.filter
 def key(d, key_name):
     """
+    Return the value of a key in a dictionary.
+
+    @type d: C{dict}
+    @param d: The given dictionary.
+    @type key_name: C{string}
+    @param key_name: The given key.
+    @rtype: C{value}
+    @return: The value of the given key in the dictionary.
     """
     if key_name in d:
         return d[key_name]
+
+
+@register.filter
+def subtract(a, b):
+    """
+    Return the difference of two integers.
+
+    @type a: C{int}
+    @param a: The number to subtract from.
+    @type b: C{int}
+    @param b: The number to subtract.
+    @rtype: C{int}
+    @return: The difference of the two given integers.
+    """
+    return a - b
+
+
+@register.filter
+def field_value(relay_dict, key_name):
+    """
+    Return the value of a specific field of a relay.
+
+    @type relay_dict: C{dict}
+    @param relay_dict: Dictionary that contains all the fields for a
+        specific relay.
+    @type key_name: C{string}
+    @param key_name: The key of a field.
+    @rtype: C{value}
+    @return: The value of the given key in the relay dictionary.
+    """
+    return relay_dict[FILTERED_NAME[key_name]]
+
+
+@register.filter
+def get_os(platform):
+    """
+    Return the icon-name of the OS.
+
+    @type platform: C{string}
+    @param platform: A string that represents the platform of the
+        relay.
+    @rtype: C{string}
+    @return: The icon-name version of the OS of the relay.
+    """
+    if platform:
+        for os in OS_LIST:
+            if os in platform:
+                if os == 'Windows' and 'Server' in platform:
+                    return 'WindowsServer'
+                else:
+                    return os
+    return 'NotAvailable'
+
